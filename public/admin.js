caf2e4e11c088e68644a4b1673aee5e9a9e4cdb4
Huntley <huntleycastner@gmail.com>
2023-02-09 18:58:28 -0800

    finished

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function main() {
 
-// Your Code Here
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+
+    let root = document.getElementById("root")
+    let listItem = document.createElement("li")
+    let qtyInput = document.createElement("input")
+    let saveBtn = document.createElement("button")
+
+    listItem.innerHTML = `<strong>${book.title}</strong>`
+    qtyInput.setAttribute('value', `${book.quantity}`) 
+    saveBtn.textContent = "Save"
+
+    saveBtn.addEventListener('click', () =>{
+        fetch('http://localhost:3001/updateBook', {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/JSON'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: qtyInput.value,
+            })
+        })
+    })
+
+    listItem.append(qtyInput, saveBtn)
+    root.append(listItem)
+}
+
+main()
